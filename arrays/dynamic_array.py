5dfac6c5fb1b7f41e0b46344e6610736c8750ce8
Devutaraka <tarakadevu1@gmail.com>
2026-03-09 19:37:34 +0530

    Update dynamic array implementation

--- arrays/dynamic_array.py
+++ arrays/dynamic_array.py
@@ -1,1 +1,26 @@
+class DynamicArray:
 
+    def __init__(self):
+        self.data = []
+
+    def append(self, value):
+        self.data.append(value)
+
+    def remove(self, value):
+        self.data.remove(value)
+
+    def get(self, index):
+        return self.data[index]
+
+    def size(self):
+        return len(self.data)
+
+
+arr = DynamicArray()
+
+arr.append(10)
+arr.append(20)
+arr.append(30)
+
+print(arr.get(1))
+print(arr.size())
